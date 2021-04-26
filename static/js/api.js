a631f72209e595946bbf0f80f3bd2a49c8a020c2
rosalinda-oliv <rosimartinsoliveira@gmail.com>
2021-04-26 18:08:40 +0100

    inicial commit

---
+++ static/js/api.js
@@ -0,0 +1,8 @@
+export default class API {
+    async getCharacter(id) {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+        const data = await response.json();
+        return data;
+   }
+}
+
