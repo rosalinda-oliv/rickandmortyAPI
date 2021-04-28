876829f1d34376c2d6c89e244022dd1f5997ae88
Rosalinda Oliveira <rosimartinsoliveira@gmail.com>
2021-04-28 14:44:42 +0100

    create a character file

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,12 @@
-import API from './api.js'
+import API from './api.js';
+import Character from './character.js';
 
-const api = new API()
-const $characterContainer = document.querySelector('#character-container')
-
-/*const characterTemplate = ` 
-    <article class="character">
-        <div class="character-grid">
-            <h2>Rick Sanchez</h2>
-            <img src="" alt="">
-        </div>
-    </article>
-`
-*/
-
-class Character {
-    constructor({name, image}) {
-        this.name = name
-        this.image= image
-        this.render()
-    }
-    build() {
-        return ` 
-        <article class="character">
-            <div class="character-grid">
-                <h2>${this.name}</h2>
-                <img src="${this.image}" alt="">
-            </div>
-        </article>
-    `
-    }
-    render() {
-        $characterContainer.innerHTML = this.build()
-    }
-}
+const api = new API();
 
 async function initApp(initCharacterId) {
     const characterData = await api.getCharacter(initCharacterId)
     console.log(characterData);
-    //const rick = new Character(characterData)
+    const rick = new Character(characterData)
 }
-initApp(1);
+initApp(4);
 //console.log(api.getCharacter(2));
